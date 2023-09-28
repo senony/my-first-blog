24229d9833b969e6b812d8f08d85b222ca9df7e0
sno_choi <seungho403@khu.ac.kr>
2023-09-28 17:09:40 +0900

    change wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+path = '/home/seungho403/my-first-blog'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
